057d639b11740870786ace2f7781cc05b8177657
Oliver Cripps <ollycripps@gmail.com>
2019-04-29 17:19:25 +0100

    Refactor to include new _isDivisibleBy method

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,13 +1,17 @@
 const fizzBuzz = function() {};
 
 fizzBuzz.prototype.isDivisibleByFifteen = function(number) {
-  return (number % 15 === 0);
+  return this._isDivisibleBy(number, 15);
 };
 
 fizzBuzz.prototype.isDivisibleByFive = function(number) {
-  return (number % 5 === 0);
+  return this._isDivisibleBy(number, 5);
 };
 
 fizzBuzz.prototype.isDivisibleByThree = function(number) {
-  return (number % 3 === 0);
+  return this._isDivisibleBy(number, 3);
+};
+
+fizzBuzz.prototype._isDivisibleBy = function(number, divisor) {
+  return (number % divisor === 0);
 };
