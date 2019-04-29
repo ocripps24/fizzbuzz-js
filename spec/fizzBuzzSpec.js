aeed4ee575e9271c60ac1610e9dcfa0baccc07c6
Oliver Cripps <ollycripps@gmail.com>
2019-04-29 16:55:05 +0100

    Refactor spec to include before block and extend first test

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,18 +2,24 @@ describe('fizzBuzz', function() {
 
   let fizzbuzz;
 
+  beforeEach(function() {
+    fizzbuzz = new fizzBuzz();
+  });
+
   describe('knows when a number is', function() {
 
     it('divisible by 3', function() {
-      fizzbuzz = new fizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(3)).toBe(true);
+    });
+
+    it('divisible by multiples of 3', function() {
+      expect(fizzbuzz.isDivisibleByThree(6)).toBe(true);
     });
 
   });
 
   describe('knows when a number is', function() {
     it('not divisible by 3', function() {
-      fizzbuzz = new fizzBuzz();
       expect(fizzbuzz.isDivisibleByThree(1)).toBe(false);
     });
   });
